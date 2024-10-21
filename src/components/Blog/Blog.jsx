5364c9bac511c1b24e2400778707af475d50fd67
ashraful2871 <ashrafulislam2758@gmail.com>
2024-10-21 20:49:43 +0600

    show blog imag,title,author name, author profile,hashtag and read time

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,42 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+  const {
+    title,
+    cover,
+    reading_time,
+    author,
+    author_img,
+    posted_date,
+    hashtags,
+  } = blog;
+  return (
+    <div>
+      <img src={cover} alt="" />
+      <div className="flex justify-between">
+        <div className="flex gap-6">
+          <img className="w-14" src={author_img} alt="" />
+          <div>
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a className="mr-2" href="">
+              #{hash}
+            </a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 Blog.propTypes = {
   blog: PropTypes.object.isRequired,
